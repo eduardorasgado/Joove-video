10fa19ea27f5ede54adaa09690e6cf74b5a30bcf
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-14 00:25:05 -0500

    Create function handleClick on media.js

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -4,9 +4,13 @@ import './media.css';
 import PropTypes from 'prop-types';
 
 class Media extends Component{
+	
+	handleClick(event){
+		window.alert("Buh!");
+	}
 	render(){
 		return(
-			<div className="Media">
+			<div className="Media" onClick={() => this.handleClick()}>
 				<div className="Media-cover">
 					<img className="Media-image" src={this.props.image} alt="" width={260} height={160}/>
 					<h3 className="Media-title">{this.props.title}</h3>
