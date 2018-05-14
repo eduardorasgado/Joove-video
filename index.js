9b6f2d6b0f779438dcc673314f8f4e8daa8a26b7
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-14 16:46:47 -0500

    Create new playlist component to render Media with playlist elements through api.json

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Media from './src/playlist/components/media';
+/*import Media from './src/playlist/components/media';*/
 import Playlist from './src/playlist/components/playlist';
 import data from './src/api.json'
+
 const container = document.getElementById("app");
-const holaReact = <Media
-						type="video"
-						title="Que chingados es el bitcoin" 
-						author="EduardoRasgado" 
-						image = "./images/covers/bitcoin.jpg"/>;
+
+var playlistComponent = <Playlist data={data} />;
 
 //Renderizamos
-ReactDOM.render(holaReact,container);
+ReactDOM.render(playlistComponent, container);
 
 /*
 npm install react react-dom --save
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import Media from './media'
 
 class Playlist extends Component {
 	render(){
+		const playlist = this.props.data.categories[0].playlist;
 		return(
-				<div></div>
+				<div>
+				{
+					playlist.map((item) => {
+						return <Media title={item.title}
+									  author={item.author}
+									  type={item.type}
+									  src={item.src}
+						/>
+					})
+				}
+
+				</div>
 			);
 	}
 }
