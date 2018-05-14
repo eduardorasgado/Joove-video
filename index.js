c172eaf3dd3fb0ed08ee7038275078dec026d1da
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-13 23:48:41 -0500

    Passed image through props

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import Media from './src/playlist/components/media';
 
 const container = document.getElementById("app");
-
-const holaReact = <Media title="Que chingados es el bitcoin" author="EduardoRasgado"/>;
+const holaReact = <Media title="Que chingados es el bitcoin" 
+						author="EduardoRasgado" 
+						image = "./images/covers/bitcoin.jpg"/>;
 
 //Renderizamos
 ReactDOM.render(holaReact,container);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -7,7 +7,7 @@ class Media extends Component{
 		return(
 			<div className="Media">
 				<div className="Media-cover">
-					<img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
+					<img className="Media-image" src={this.props.image} alt="" width={260} height={160}/>
 					<h3 className="Media-title">{this.props.title}</h3>
 					<p className="Media-author">{this.props.author}</p>
 				</div>
