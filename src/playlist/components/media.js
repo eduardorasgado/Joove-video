947cd5c1119679200c3187dd1469553ec7e7a9f4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-13 23:07:28 -0500

    Se agregan estilos

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class Media extends Component{
 	render(){
+		const styles = {
+			container: {
+				fontSize: "14px",
+				backgroundColor: "white",
+				color:"#44546b",
+				fontFamily: "lato",
+				cursor:"pointer",
+			}
+		}
 		return(
-			<div>
+			<div style={styles.container}>
 				<div>
 					<img src="" alt="" width={260} height={160}/>
 					<h3>Estoy creando un componente de React</h3>
