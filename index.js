84616e4a57207fa103a2107ac5c0cecd345dd2a4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-13 22:14:45 -0500

    media.js component for playlist created and imported at index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media';
 
 const container = document.getElementById("app");
 
-const holaReact = <h1>Hola React</h1>;
+const holaReact = <Media />;
 
 //Renderizamos
 ReactDOM.render(holaReact,container);
+
+/*
+npm install react react-dom --save
+*/
---
+++ src/playlist/components/media.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Media extends Component{
+	render(){
+		return(
+			<div>
+				<div>
+					<img src="" alt="" width={260} height={160}/>
+					<h3>Estoy creando un componente de React</h3>
+					<p>EduardoRasgado</p>
+				</div>
+			</div>
+			);
+	}
+}
+
+export default Media;
