b2120f3178b3273991b8979cfe917033ca882d57
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-13 23:39:21 -0500

    Added new styles to media.js

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
+import './media.css';
+
 
 class Media extends Component{
 	render(){
-		const styles = {
-			container: {
-				fontSize: "14px",
-				backgroundColor: "white",
-				color:"#44546b",
-				fontFamily: "lato",
-				cursor:"pointer",
-				width: "260px",
-				border: "1px solid red",
-				padding: "6px",
-				margin: "4px",
-			}
-		}
 		return(
-			<div style={styles.container}>
-				<div>
-					<img src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
-					<h3>Estoy creando un componente de React</h3>
-					<p>EduardoRasgado</p>
+			<div className="Media">
+				<div className="Media-cover">
+					<img className="Media-image" src="./images/covers/bitcoin.jpg" alt="" width={260} height={160}/>
+					<h3 className="Media-title">Estoy creando un componente de React</h3>
+					<p className="Media-author">EduardoRasgado</p>
 				</div>
 			</div>
 			);
