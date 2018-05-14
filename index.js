340385520fd74c5286f9a4f35cc5c4c15ce8a4e5
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-14 15:22:22 -0500

    Created playlist component to bring api.json on

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Media from './src/playlist/components/media';
-
+import Playlist from './src/playlist/components/playlist';
+import data from './src/api.json'
 const container = document.getElementById("app");
 const holaReact = <Media
 						type="video"
