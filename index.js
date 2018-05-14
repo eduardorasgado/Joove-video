47e85c0b6a83734825d9c95c2160faab8b7b1ba3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-13 21:26:48 -0500

    React and ReactDOM installed and added to dependencies, rendered from index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello React");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById("app");
+
+const holaReact = <h1>Hola React</h1>;
+
+//Renderizamos
+ReactDOM.render(holaReact,container);
