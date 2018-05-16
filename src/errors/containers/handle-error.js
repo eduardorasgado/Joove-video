332163def37df9fbc0faa73f4757b50c0309fd2f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-16 05:56:13 -0500

    Handling errors from component home, handle-error component created

---
+++ src/errors/containers/handle-error.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class handleError extends Component{
+	render(){
+		return(
+			<div>
+				blabla
+			</div>
+		);
+	}
+}
+
+export default handleError;
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -8,20 +8,33 @@ import Modal from '../../widgets/components/modal-component';
 class Home extends Component {
 	state = {
 			modalVisible: false,
+			handleError: false,
 	}
 
 	handleOpenModal = () => {
 		this.setState({
 			modalVisible: true,
-		})
+		});
 	}
 
 	handleCloseModal = (event) => {
 		this.setState({
 			modalVisible: false,
-		})
+		});
 	}
-	render(){
+
+	componentDidCatch(error, info) {
+		//if an error occurs
+		this.setState({
+			handleError: true
+		});
+	}
+
+	render() {
+		if (this.state.handleError) {
+			return <h3>Oops! Algo salió mal, lo sentimos mucho :c porque no recargas desde el inicio?</h3>;
+		}
+
 		const categoriesData  = this.props.data.categories;
 		return(
 			<HomeLayout>
