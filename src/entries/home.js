f7080d299ab9ebf6de0544facac3783c7f492abe
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-16 03:13:27 -0500

    ModalContainer component created

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import Home from '../pages/containers/home'
 import data from '../api.json';
 
-const container = document.getElementById("app");
+const homeContainer = document.getElementById("home-container");
 
 //Llamada a containers/home.js
 var homeEntry = <Home data={data} />
 
 //Renderizamos
-ReactDOM.render(homeEntry, container);
+ReactDOM.render(homeEntry, homeContainer);
 
 /*
 npm install react react-dom --save
---
+++ src/pages/containers/modal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ModalContainer extends Component{
+	render(){
+		return(
+				<div>
+					hello
+				</div>
+			);
+	}
+}
+
+export default ModalContainer;
