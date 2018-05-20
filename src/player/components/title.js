40f1de04deef41e6476a1a4b5ceade64950c3e0f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-20 03:58:49 -0500

    Added styles to video player

---
+++ src/player/components/title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './title.css';
+
+const Title = (props) => (
+		<div className="Title">
+			<h3>{props.title}</h3>
+		</div>
+	);
+
+export default Title; 
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout.js';
-import Video from '../components/video.js';
+import Video from '../components/video';
+import Title from '../components/title';
 
 class VideoPlayerContainer extends Component{
 	render(){
 		return(
 				<VideoPlayerLayout>
+					<Title title={"Hola Enfermera"}/>
 					<Video 
 					autoplay={false}
 						src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
