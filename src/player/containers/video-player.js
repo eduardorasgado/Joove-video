81f5e3f449df331a1cac676727710284f85976a6
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-19 22:36:36 -0500

    Created Video.js component for video UI

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout.js';
+import Video from '../components/video.js';
 
 class VideoPlayerContainer extends Component{
 	render(){
 		return(
 				<VideoPlayerLayout>
-					<video 
-					controls
-					autoPlay
-					src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-					/>
+					<Video />
 				</VideoPlayerLayout>
 			);
 	}
