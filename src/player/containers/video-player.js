e13bdcefcd44ec4b72e6ef07730a8c730c5004ac
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-20 21:39:37 -0500

    CUrrent time added logic and to timer copmponent

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -43,8 +43,18 @@ class VideoPlayerContainer extends Component{
 	}
 
 	handleTimeMediaUpdate = (event) => {
-		console.log(parseInt(this.video.currentTime))
-		this.setState({currentTime:	this.video.currentTime,
+		var sec_num = parseInt(this.video.currentTime);
+
+		var hours   = Math.floor(sec_num / 3600);
+	    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+	    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+	    if (hours   < 10) {hours   = "0"+hours;}
+	    if (minutes < 10) {minutes = "0"+minutes;}
+	    if (seconds < 10) {seconds = "0"+seconds;}
+		var currTime = hours+":"+minutes+":"+seconds;
+
+		this.setState({currentTime:	currTime,
 		});
 	}
 
@@ -59,6 +69,7 @@ class VideoPlayerContainer extends Component{
 						/>
 						<Timer 
 							duration={this.state.duration}
+							currentTime ={this.state.currentTime} 
 						/>
 					</VideoPlayerControls>
 					<Video 
